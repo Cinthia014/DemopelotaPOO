fa0421f884f39b7a48accfc1c91572cbb63db514
Cinthia014 <150810744+Cinthia014@users.noreply.github.com>
2024-05-14 10:46:16 -0500

    Update mi juego.py
    cambio en la ejecución del juego

--- mi juego.py
+++ mi juego.py
@@ -8,7 +8,11 @@ screen = pygame.display.set_mode((800, 600)) # tamaño (ancho, alto)
 p = Pelota()
 
 clock = pygame.time.Clock() # <--- crear un reloj
-while True:
+ game_over = False
+while not  game_over:
+        for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
     clock.tick(50) # <--- limitar que el juego se ejecute 50 veces por segundo
     screen.fill((0, 0, 0)) # <--- fondo
     screen.blit(p.ball, p.ball_rect) # dibujar la pelota
